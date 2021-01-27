2447fb95c7b4e0947890c77db987c4dd4495fab4
Geon George <geongeorge98@gmail.com>
2021-01-27 17:23:04 +0530

    Adds tooltip

--- src/components/app/GridArea.js
+++ src/components/app/GridArea.js
@@ -1,5 +1,6 @@
 /** @format */
 import SVG from "react-inlinesvg";
+import ReactTooltip from "react-tooltip";
 import Arrow from "../../assets/arrow-right.svg";
 
 const WEEKS_IN_YEAR = 52;
@@ -9,10 +10,14 @@ function GridArea() {
   const age = 23;
   const ageWeeks = age * WEEKS_IN_YEAR;
 
+  function getCurrentWeek(week, year) {
+    return year * WEEKS_IN_YEAR + (week + 1);
+  }
+
   function getWeekClass(week, year) {
     let className = "life-grid__el";
 
-    const isActive = year * WEEKS_IN_YEAR + week <= ageWeeks;
+    const isActive = getCurrentWeek(week, year) <= ageWeeks;
     if (isActive) {
       className += " life-grid__el--active";
     }
@@ -20,79 +25,88 @@ function GridArea() {
   }
 
   return (
-    <div className="life-grid-wrapper">
-      {/* Row 1 */}
-      <div></div>
-      <div></div>
-      <div className="life-grid-wrapper__weeks">
-        <div>
-          <span>Weeks </span>
+    <div>
+      <div className="life-grid-wrapper">
+        {/* Row 1 */}
+        <div></div>
+        <div></div>
+        <div className="life-grid-wrapper__weeks">
           <div>
-            <SVG src={Arrow}></SVG>
+            <span>Weeks </span>
+            <div>
+              <SVG src={Arrow}></SVG>
+            </div>
           </div>
         </div>
-      </div>
-      {/* End Row 1 */}
+        {/* End Row 1 */}
 
-      {/* Row 2 */}
-      <div></div>
-      <div></div>
-      <div className="life-grid">
-        <div className="life-grid__row">
-          {Array.from({ length: WEEKS_IN_YEAR }, (_, week) => {
-            const current = week + 1;
+        {/* Row 2 */}
+        <div></div>
+        <div></div>
+        <div className="life-grid">
+          <div className="life-grid__row">
+            {Array.from({ length: WEEKS_IN_YEAR }, (_, week) => {
+              const current = week + 1;
+              let classes = "life-grid__el life-grid__label";
+              classes +=
+                current === 1 || current % 5 === 0
+                  ? " life-grid__label--show"
+                  : "";
+
+              return (
+                <span className={classes} key={week}>
+                  {current}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+        {/* Row 2 end */}
+
+        {/* Row 3 */}
+        <div className="life-grid-wrapper__years">
+          <div>
+            <span>Years </span>
+            <div>
+              <SVG src={Arrow}></SVG>
+            </div>
+          </div>
+        </div>
+
+        <div className="life-grid">
+          {Array.from({ length: TOTAL_YEARS }, (_, year) => {
+            const current = year + 1;
             let classes = "life-grid__el life-grid__label";
             classes +=
               current === 1 || current % 5 === 0
                 ? " life-grid__label--show"
                 : "";
-
             return (
-              <span className={classes} key={week}>
-                {current}
-              </span>
+              <div className="life-grid__row">
+                <span className={classes} key={year} data-tip="hello world">
+                  {current}
+                </span>
+              </div>
             );
           })}
         </div>
-      </div>
-      {/* Row 2 end */}
 
-      {/* Row 3 */}
-      <div className="life-grid-wrapper__years">
-        <div>
-          <span>Years </span>
-          <div>
-            <SVG src={Arrow}></SVG>
-          </div>
-        </div>
-      </div>
-
-      <div className="life-grid">
-        {Array.from({ length: TOTAL_YEARS }, (_, year) => {
-          const current = year + 1;
-          let classes = "life-grid__el life-grid__label";
-          classes +=
-            current === 1 || current % 5 === 0 ? " life-grid__label--show" : "";
-          return (
-            <div className="life-grid__row">
-              <span className={classes} key={year}>
-                {current}
-              </span>
+        <section className="life-grid">
+          {Array.from({ length: TOTAL_YEARS }, (_, year) => (
+            <div className="life-grid__row" key={year}>
+              {Array.from({ length: WEEKS_IN_YEAR }, (_, week) => (
+                <div
+                  className={getWeekClass(week, year)}
+                  key={week}
+                  data-tip={`Year ${year + 1} Week ${week}`}
+                ></div>
+              ))}
             </div>
-          );
-        })}
+          ))}
+        </section>
+        {/* Row 3 end */}
       </div>
-
-      <section className="life-grid">
-        {Array.from({ length: TOTAL_YEARS }, (_, year) => (
-          <div className="life-grid__row" key={year}>
-            {Array.from({ length: WEEKS_IN_YEAR }, (_, week) => (
-              <span className={getWeekClass(week, year)} key={week}></span>
-            ))}
-          </div>
-        ))}
-      </section>
-      {/* Row 3 end */}
+      <ReactTooltip />
     </div>
   );
 }
